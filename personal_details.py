9cdfc8aa91757f690473437859c89633757e7ebe
Chris Black <31255096+Ckoria@users.noreply.github.com>
2024-03-23 02:31:20 +0200

    Update personal_details.py

--- personal_details.py
+++ personal_details.py
@@ -21,7 +21,6 @@ def projects():
     return {
         "Mawox Work Management System (Credentials on request)": "https://mawox.pythonanywhere.com",
         "On the Move App Admin Website  (Credentials on request)": "https://mmallonthemove.co.za/",
-        "Mawox Excel Inventory System  (Github Repo with Excel File)": "https://github.com/Ckoria/excel_vba_pos",
         "Mawox Google Sheets Inventory System": "https://docs.google.com/spreadsheets/d/1pw1ce5nJkPKr24XKhNC6MyzDzwRb8uiRCbPDB_WtsUU/edit#gid=1641138601",
         "Google Sheets and RepairShopr API Testing (Github Repo)": "https://github.com/Ckoria/APIs-Testing",
         "MM All Status Tracker": "https://ckoria-ha-customers-get-customers-5w5cdv.streamlit.app/",
