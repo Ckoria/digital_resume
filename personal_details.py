fdea5c014b5817fb7e0569dbe935bcbc952573ce
Chris Black <31255096+Ckoria@users.noreply.github.com>
2024-03-23 02:08:12 +0200

    Update personal_details.py

--- personal_details.py
+++ personal_details.py
@@ -19,7 +19,7 @@ def social_media():
     
 def projects():
     return {
-        "Mawox Work Management System": "https://mawox.pythonanywhere.com",
+        "Mawox Work Management System (Credentials on request)": "https://mawox.pythonanywhere.com",
         "On the Move App Admin Website  (Credentials on request)": "https://mmallonthemove.co.za/",
         "Mawox Excel Inventory System  (Github Repo with Excel File)": "https://github.com/Ckoria/excel_vba_pos",
         "Mawox Google Sheets Inventory System": "https://docs.google.com/spreadsheets/d/1pw1ce5nJkPKr24XKhNC6MyzDzwRb8uiRCbPDB_WtsUU/edit#gid=1641138601",
