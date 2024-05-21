fcce12f3df3f8c44117cdceacf7c9aa3fea0d1b4
Chris Black <31255096+Ckoria@users.noreply.github.com>
2024-05-21 09:16:51 +0200

    Update education.py

--- education.py
+++ education.py
@@ -3,8 +3,10 @@ import streamlit as st
 def edu_info():
     return  [
         ['WeThinkCode | Software Engineering', '24 Months (2023 - 2024)', 
-         'In Progress', 'https://www.wethinkcode.co.za/contact'], 
-        ["Cisco | Creating Compelling Reports", "1 Months (MAR 2024)", 
+         'In Progress', 'https://www.wethinkcode.co.za/contact'],
+        ["IBM SkillsBuild | Data Fundamentals", "1 Month (MAY 2024)", 
+         "Completed", 'https://www.credly.com/badges/2123f044-31c5-49be-8b1d-60e8e8f87225/public_url'], 
+        ["Cisco | Creating Compelling Reports", "1 Month (MAR 2024)", 
          "Completed", 'https://www.credly.com/badges/2123f044-31c5-49be-8b1d-60e8e8f87225/public_url'], 
         ["Coursera (AWS)  | AWS Cloud Technical Essentials", "1 Month (MAR 2024)", 
          "Completed", "https://coursera.org/share/0dda5be89b54507260c8f52320b391bb"], 
@@ -14,9 +16,9 @@ def edu_info():
          "Completed", "https://www.credly.com/badges/a4b286ad-26e7-439c-a166-a862298932f2/public_url"], 
         [" Coursera (The Hong Kong University of Science and Technology) | Python and Statistics for Financial Analysis",
          "1 Month (AUG 2023)", "Completed", "https://coursera.org/share/e6b7f2942b30e50c32c2dfb560db3c31"], 
-        ["Coursera  | IBM Intro to Data Science", "1 Months (FEB 2022)", 
+        ["Coursera  | IBM Intro to Data Science", "1 Month (FEB 2022)", 
          "Completed", "https://coursera.org/share/1f4afb233376528b9c25f58aced5c229"], 
-        ["University of the Witswatersrand | BSc Computer Science", " 1 year (2015)", "Discontinued", ""],
+        ["The University of the Witswatersrand | BSc Computer Science", " 1 year (2015)", "Discontinued", ""],
         ["Isibani Academy | Grade 12", "1 year (2014)", "Completed", ""]]
 
 def education():
